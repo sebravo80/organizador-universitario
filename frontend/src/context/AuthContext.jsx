7c2e1bc03be2fc00125d4879e9ffc08c1040c211
sebravo80 <sebastian.bravo80@inacapmail.cl>
2025-04-15 22:24:22 -0400

    probando despliegue

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -21,7 +21,7 @@ export const AuthProvider = ({ children }) => {
       console.log('Intentando iniciar sesión con:', { email });
       
       // Usar fetch directamente para ver más detalles
-      const response = await fetch('http://localhost:5000/api/auth', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL || 'http://localhost:5000/api'}/auth`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
