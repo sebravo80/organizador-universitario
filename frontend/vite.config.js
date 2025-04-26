1d251487b6ea64272eec4a83c7054fff3827bf44
sebravo80 <sebastian.bravo80@inacapmail.cl>
2025-04-25 22:01:54 -0400

    se me olvidó algo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Cambia esto si es necesario
+    port: 5173, 
   },
 });
