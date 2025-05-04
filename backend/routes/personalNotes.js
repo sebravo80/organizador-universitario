9e1a4039ba67df22e2a2021c0eefb56f7ca09260
sebravo80 <sebastian.bravo80@inacapmail.cl>
2025-05-03 23:02:00 -0400

    probando nueva pag 2

--- backend/routes/personalNotes.js
+++ backend/routes/personalNotes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const PersonalNote = require('../models/PersonalNote');
+const PersonalNote = require('../models/PersonalNotes');
 
 // Obtener todas las notas del usuario
 router.get('/', auth, async (req, res) => {
